8049409468e70e72b118acc69b883850903ee103
Yonatan Rojas <yonatan@bedu.org>
2018-03-13 21:20:35 -0600

    added dependencies for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
-import {AngularFirestoreModule} from 'angularfire2/firestore',
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 
