ad5310b0c847d77ea3d4ba0b3eeb430e9ad43bab
Yonatan Rojas <yonatan@bedu.org>
2018-03-15 20:49:34 -0600

    added material shared module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,12 +5,15 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../../environments/environment';
 import { AuthService } from './services/auth.service';
 import { LoginComponent } from './login/login.component';
+import { MaterialSharedModule } from '../material-shared/material-shared.module';
+
 
 @NgModule({
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
+    MaterialSharedModule
   ],
   declarations: [LoginComponent],
   exports: [
